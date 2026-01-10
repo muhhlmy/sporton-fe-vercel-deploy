030680526488c27bebdb49b9b430d9962a9ede99
muhhlmy <muhhlmy@gmail.com>
2026-01-10 09:02:28 +0700

    Update

--- app/(landing)/components/home/categories.tsx
+++ app/(landing)/components/home/categories.tsx
@@ -1,18 +1,19 @@
+import Image from "next/image";
 import Link from "next/link";
 import { FiArrowRight } from "react-icons/fi";
 
 const categoryList = [
-  { name: "badminton", imgUrl: "category-badminton.png" },
-  { name: "basketball", imgUrl: "category-basketball.png" },
-  { name: "football", imgUrl: "category-football.png" },
-  { name: "running", imgUrl: "category-running.png" },
-  { name: "swimming", imgUrl: "category-swimming.png" },
-  { name: "tennis", imgUrl: "category-tennis.png" },
+  { name: "Badminton", imgUrl: "category-badminton.png" },
+  { name: "Basketball", imgUrl: "category-basketball.png" },
+  { name: "Football", imgUrl: "category-football.png" },
+  { name: "Running", imgUrl: "category-running.png" },
+  { name: "Swimming", imgUrl: "category-swimming.png" },
+  { name: "Tennis", imgUrl: "category-tennis.png" },
 ];
 
 const CategoriesSection = () => {
   return (
-    <section id="category-section" className="container mx-auto">
+    <section id="category-section" className="container mx-auto pb-20">
       <div className="flex justify-between">
         <h2 className="font-bold text-2xl">Browse by Categories</h2>
         <Link href="#" className="flex gap-2 text-primary">
@@ -20,8 +21,26 @@ const CategoriesSection = () => {
           <FiArrowRight className="self-center" />
         </Link>
       </div>
-      <div className="grid grid-cols-6 gap-12">
-        <div className="rounded-lg bg-linear-to-r  from-[#F1F1F1] to-[#F7F7F7] w-full aspect-square"></div>
+      <div className="grid grid-cols-6 gap-12 mt-8">
+        {categoryList.map((category, index) => (
+          <div
+            className="rounded-lg bg-linear-to-r  from-[#F1F1F1] to-[#F7F7F7] w-full aspect-square flex justify-center"
+            key={index}
+          >
+            <div className="self-center">
+              <Image
+                src={`/images/categories/${category.imgUrl}`}
+                width={86}
+                height={86}
+                alt="Categories Icon"
+                className="mb-[10px]"
+              />
+              <div className="text-primary font-meidium text-xl text-center">
+                {category.name}
+              </div>
+            </div>
+          </div>
+        ))}
       </div>
     </section>
   );
