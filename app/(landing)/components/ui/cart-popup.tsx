5193c46cfb59bc7950de92d97d4c0e63e03c56ab
muhhlmy <muhhlmy@gmail.com>
2026-01-18 12:15:43 +0700

    Menambahkan dukungan untuk menampilkan jumlah item di keranjang belanja dan menampilkan pesan ketika keranjang kosong. Memperbarui komponen Header dan CartPopup untuk menggunakan data dari store.

--- app/(landing)/components/ui/cart-popup.tsx
+++ app/(landing)/components/ui/cart-popup.tsx
@@ -95,34 +95,45 @@ const CartPopup = () => {
         Shopping Cart
       </div>
       <div className="overflow-auto max-h-75">
-        {items.map((item, index) => (
-          <div className="border-b border-gray-200 p-4 flex gap-3" key={index}>
-            <div className="bg-primary-light aspect-square w-16 flex justify-center items-center">
-              <Image
-                src={getImageUrl(item.imageUrl)}
-                alt={item.name}
-                width={63}
-                height={63}
-                className="aspect-square object-contain"
-              />
-            </div>
-            <div className="self-center w-full">
-              <div className="text-sm font-medium">{item.name}</div>
-              <div className="flex gap-3 font-medium text-xs">
-                <div>{item.qty}x</div>
-                <div className="text-primary">{priceFormatter(item.price)}</div>
-              </div>
-            </div>
-            <Button
-              size="small"
-              variant="ghost"
-              className="w-7 h-7 p-0! self-center mx-auto"
-              onClick={() => removeItem(item._id)}
+        {items.length ? (
+          items.map((item, index) => (
+            <div
+              className="border-b border-gray-200 p-4 flex gap-3"
+              key={index}
             >
-              <FiTrash2 />
-            </Button>
+              <div className="bg-primary-light aspect-square w-16 flex justify-center items-center">
+                <Image
+                  src={getImageUrl(item.imageUrl)}
+                  alt={item.name}
+                  width={63}
+                  height={63}
+                  className="aspect-square object-contain"
+                />
+              </div>
+              <div className="self-center w-full">
+                <div className="text-sm font-medium">{item.name}</div>
+                <div className="flex gap-3 font-medium text-xs">
+                  <div>{item.qty}x</div>
+                  <div className="text-primary">
+                    {priceFormatter(item.price)}
+                  </div>
+                </div>
+              </div>
+              <Button
+                size="small"
+                variant="ghost"
+                className="w-7 h-7 p-0! self-center mx-auto"
+                onClick={() => removeItem(item._id)}
+              >
+                <FiTrash2 />
+              </Button>
+            </div>
+          ))
+        ) : (
+          <div className="text-center py-5 opacity-50">
+            Your shopping cart is empty
           </div>
-        ))}
+        )}
       </div>
       <div className="border-t border-gray-200 p-4">
         <div className="font-semibold flex justify-between items-center">
