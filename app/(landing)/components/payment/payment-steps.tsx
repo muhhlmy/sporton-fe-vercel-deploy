cbfaa33ceccc650cca70d102ffc20ac6c660ac5d
muhhlmy <muhhlmy@gmail.com>
2026-01-20 10:19:27 +0700

    Menambahkan fungsionalitas untuk proses pembayaran, termasuk pengunggahan bukti pembayaran dan konfirmasi transaksi. Memperbarui komponen OrderStatus untuk menampilkan status transaksi dan menambahkan komponen OrderRejected untuk menangani pembayaran yang ditolak. Menambahkan dukungan untuk pengambilan transaksi berdasarkan ID dan memperbarui tipe data Transaction.

--- app/(landing)/components/payment/payment-steps.tsx
+++ app/(landing)/components/payment/payment-steps.tsx
@@ -6,11 +6,60 @@ import CardWithHeader from "../ui/card-with-header";
 import FileUpload from "../ui/file-upload";
 import priceFormatter from "@/app/utils/price-formater";
 import { useRouter } from "next/navigation";
+import { useState } from "react";
+import { useCartStore } from "@/app/hooks/use-cart-store";
+import { transactionCheckout } from "@/app/services/transaction.services";
 
 const PaymentSteps = () => {
   const { push } = useRouter();
+  const { customerInfo, items, reset } = useCartStore();
+  const [file, setFile] = useState<File | null>();
+
+  const totalPrice = items.reduce(
+    (total, item) => total + item.price * item.qty,
+    0,
+  );
+
   const uploadAndConfirm = () => {
     push("/order-status/1");
+  };
+
+  const handleConfirmPayment = async () => {
+    if (!file) {
+      alert("Please upload your payment receipt!");
+      return;
+    }
+
+    if (!customerInfo) {
+      alert("Please return to checkout");
+      push("/checkout");
+      return;
+    }
+
+    try {
+      const formData = new FormData();
+      formData.append("customerName", customerInfo.customerName);
+      formData.append(
+        "customerContact",
+        customerInfo.customerContact!.toString(),
+      );
+      formData.append("customerAddress", customerInfo.customerAddress);
+      formData.append("image", file);
+      formData.append(
+        "purchasedItems",
+        JSON.stringify(
+          items.map((item) => ({ productId: item._id, qty: item.qty })),
+        ),
+      );
+      formData.append("totalPayment", totalPrice!.toString());
+      const res = await transactionCheckout(formData);
+      // console.log("Transaction Response: ", res);
+      alert("Transaction created successfully!");
+      reset();
+      push(`/order-status/${res._id}`);
+    } catch (error) {
+      console.log(error);
+    }
   };
 
   return (
@@ -33,17 +82,19 @@ const PaymentSteps = () => {
             your transaction.
           </li>
         </ol>
-        <FileUpload />
+        <FileUpload onFileSelect={setFile} />
       </div>
       <div className="border-t border-gray-200 p-4">
         <div className="font-semibold flex justify-between items-center">
           <div className="text-sm">Total</div>
-          <div className="text-primary text-xs">{priceFormatter(1045000)}</div>
+          <div className="text-primary text-xs">
+            {priceFormatter(totalPrice)}
+          </div>
         </div>
         <Button
           variant="dark"
           className="w-full mt-4"
-          onClick={uploadAndConfirm}
+          onClick={handleConfirmPayment}
         >
           <FiCheckCircle /> Upload Receipt & Confirm
         </Button>
